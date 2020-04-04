8c8a9e1ce162d88cbcde9a0a38f2bfddb1bb5b43
Ricardo Ferreira da Silva <ricardoofficiall@gmail.com>
2020-04-04 12:16:43 +0100

    Initial commit and setup.

---
+++ Entrepreneur/Entrepreneur/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrepreneur
+{
+    public class Class1
+    {
+    }
+}
